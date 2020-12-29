62ba13c00207c6b2b1f1fa2349284d8d00b11839
Lionel Beard <lionel@beard.fr>
2020-12-30 00:26:46 +0100

    on/off instead of true/false

--- ioniq_main.sh
+++ ioniq_main.sh
@@ -12,8 +12,8 @@ PIJUICE_BATT=`python3 /home/pi/EV_post_status/pijuice_getchargelevel.py | sed "s
 
 curl -X POST -H "Content-Type: application/json" -d '{"soc":"'"$SOC"'", "date":"'"$DATE"'", "pijuice_batt":"'"$PIJUICE_BATT"'"}' http://domobeyou.beard.fr:1980/ioniq-status
 
-donthalt=`curl -X GET http://domobeyou.beard.fr:1980/dont-halt`
-if [ $donthalt == "FALSE" ]
+autohalt=`curl -X GET http://domobeyou.beard.fr:1980/auto-halt`
+if [ $autohalt == "on" ]
 then
   python3 /home/pi/EV_post_status/shutdown.py
 else
