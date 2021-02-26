5eea9d5a536832140a16cd248d77b1f5d072167e
Lionel Beard <lionel@beard.fr>
2021-02-26 16:10:06 +0100

    only pijuice status

--- ioniq_main.sh
+++ ioniq_main.sh
@@ -1,22 +1,28 @@
 #!/bin/bash
 
 
-SOC=`python /home/pi/EV_post_status/ioniq_soc.py`
+#SOC=`python /home/pi/EV_post_status/ioniq_soc.py`
 
 # Sync RTC time
-sudo hwclock -s
+#sudo hwclock -s
 
 DATE=`date`
 
-PIJUICE_BATT=`python3 /home/pi/EV_post_status/pijuice_getchargelevel.py | sed "s/'/\"/g" | jq '.data'`
+PIJUICE_CMD=`python3 /home/pi/EV_post_status/pijuice_getchargelevel.py`
+PIJUICE_BATT=`echo $PIJUICE_CMD | sed "s/'/\"/g" | jq '.data'`
+PIJUICE_ERROR=`echo $PIJUICE_CMD | sed "s/'/\"/g" | jq '.error' | sed 's/"//g'`
 
-curl -X POST -H "Content-Type: application/json" -d '{"soc":"'"$SOC"'", "date":"'"$DATE"'", "pijuice_batt":"'"$PIJUICE_BATT"'"}' http://domobeyou.beard.fr:1980/ioniq-status
+echo $PIJUICE_BATT
+echo $PIJUICE_ERROR
 
-autohalt=`curl -X GET http://domobeyou.beard.fr:1980/auto-halt`
-if [ $autohalt == "on" ]
-then
-  python3 /home/pi/EV_post_status/shutdown.py
-else
-  echo "Not halting..."
-  sudo systemctl start pioniq.timer
-fi
+curl -X POST -H "Content-Type: application/json" -d '{"date":"'"$DATE"'", "pijuice_batt":"'"$PIJUICE_BATT"'", "pijuice_error":"'"$PIJUICE_ERROR"'"}' http://domobeyou.beard.fr:1980/pijuice-status
+#curl -X POST -H "Content-Type: application/json" -d '{"soc":"'"$SOC"'", "date":"'"$DATE"'", "pijuice_batt":"'"$PIJUICE_BATT"'"}' http://domobeyou.beard.fr:1980/ioniq-status
+
+#autohalt=`curl -X GET http://domobeyou.beard.fr:1980/auto-halt`
+#if [ $autohalt == "on" ]
+#then
+#  python3 /home/pi/EV_post_status/shutdown.py
+#else
+#  echo "Not halting..."
+#  sudo systemctl start pioniq.timer
+#fi
--- ioniq_soc.py
+++ ioniq_soc.py
@@ -18,15 +18,19 @@ while True:
     reading = ser.read()
     seq.append(reading)
     joineddata = ' '.join(str(v) for v in seq).replace(' ', '')
-
+    print joineddata
     err = re.search('ERROR', joineddata)
     if err:
         break
     m = re.search('4([^;]*)5:', joineddata) #'/4([^;]*)\n5', joineddata)
     if m:
         ser.close()
+        print ('m= ')
         test = str(m.group(0))
-        x = (test[-8:])      
+        print test
+        x = (test[-8:])
+        print x
+        print x[3:5]
         SoC = (int( x[3:5], 16)/2)
         if SoC > 0 & SoC <= 100:
             print(SoC)
